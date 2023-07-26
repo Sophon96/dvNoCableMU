59e5e1188cb8d681a5e7f4c02083b42ecb9342fb
Sophon96 <71684640+Sophon96@users.noreply.github.com>
2023-07-26 12:16:27 -0700

    feat: basic implementation for bepinex

---
+++ LocoWrapper.cs
@@ -0,0 +1,320 @@
+﻿using System;
+using DV.Simulation.Cars;
+using DV.ThingTypes;
+using LocoSim.Implementations;
+
+namespace MyFirstPlugin;
+
+public class LocoWrapper: IEquatable<LocoWrapper>
+{
+    private readonly TrainCar _loco;
+
+    private SimController _simController;
+
+    private readonly SimulationFlow _simFlow;
+
+    // Common to all locomotives
+    private readonly Port _brakePort,
+        _engineOnPort,
+        _fuelConsumptionPort,
+        _maxRpmPort, // TODO: remove?
+        _rpmPort,
+        _rpmNormPort, // TODO: remove?
+        _fuelNormPort,
+        _fuelCapPort,
+        _indBrakePort,
+        _oilNormPort,
+        _oilCapPort,
+        _reverserPort,
+        _sandNormPort,
+        _sandCapPort,
+        _sanderPort,
+        _throttlePort;
+
+    // technically common to all locomotives, but hydraulic and electric have different impl
+    private readonly Port _tempPort;
+
+    // Electric stuff
+    private readonly Port? _tmAmpsPort,
+        _tmAmpsNormPort,
+        _tmMaxAmpsPort,
+        _tmRpmPort,
+        _tmRpmNormPort,
+        _tmFusePort,
+        _tmStatePort;
+
+    // DE6 and DH4 only
+    private readonly Port? _dynBrakePort;
+
+    // Hydro stuff
+    private readonly Port? _fcRpmPort,
+        _fcRpmNormPort,
+        _fcBrokenPort,
+        _fcActiveConfigPort, // TODO: remove?
+        _fcEfficiencyPort; // TODO: remove?
+
+
+    public TrainCarType Type { get; }
+    public Trainset Trainset { get; private set; }
+
+
+    public LocoWrapper(TrainCar loco)
+    {
+        _loco = loco;
+        _simController = loco.GetComponent<SimController>();
+        _simFlow = _simController.simFlow;
+
+        Type = loco.carType;
+
+        Trainset = loco.trainset;
+        loco.TrainsetChanged += OnTrainsetChanged;
+
+        _simFlow.TryGetPort("brake.EXT_IN", out _brakePort);
+        _simFlow.TryGetPort("de.ENGINE_ON", out _engineOnPort);
+        _simFlow.TryGetPort("de.FUEL_CONSUMPTION_NORMALIZED", out _fuelConsumptionPort);
+        _simFlow.TryGetPort("de.MAX_RPM", out _maxRpmPort);
+        _simFlow.TryGetPort("de.RPM", out _rpmPort);
+        _simFlow.TryGetPort("de.RPM_NORMALIZED", out _rpmNormPort);
+        _simFlow.TryGetPort("fuel.NORMALIZED", out _fuelNormPort);
+        _simFlow.TryGetPort("fuel.CAPACITY", out _fuelCapPort);
+        _simFlow.TryGetPort("indBrake.EXT_IN", out _indBrakePort);
+        _simFlow.TryGetPort("oil.NORMALIZED", out _oilNormPort);
+        _simFlow.TryGetPort("oil.CAPACITY", out _oilCapPort);
+        _simFlow.TryGetPort("reverser.CONTROL_EXT_IN", out _reverserPort);
+        _simFlow.TryGetPort("sand.NORMALIZED", out _sandNormPort);
+        _simFlow.TryGetPort("sand.CAPACITY", out _sandCapPort);
+        _simFlow.TryGetPort("sander.CONTROL_EXT_IN", out _sanderPort);
+        _simFlow.TryGetPort("throttle.EXT_IN", out _throttlePort);
+
+        switch (Type)
+        {
+            case TrainCarType.LocoDiesel:
+                _simFlow.TryGetPort("dynamicBrake.EXT_IN", out _dynBrakePort);
+                goto case TrainCarType.LocoShunter;
+
+            case TrainCarType.LocoShunter:
+                _simFlow.TryGetPort("tmHeat.TEMPERATURE", out _tempPort);
+
+                _simFlow.TryGetPort("tm.AMPS", out _tmAmpsPort);
+                _simFlow.TryGetPort("tm.AMPS_NORMALIZED", out _tmAmpsNormPort);
+                _simFlow.TryGetPort("tm.MAX_AMPS", out _tmMaxAmpsPort);
+                _simFlow.TryGetPort("tm.RPM", out _tmRpmPort);
+                _simFlow.TryGetPort("tm.RPM_NORMALIZED", out _tmRpmNormPort);
+                _simFlow.TryGetPort("tm.OVERHEAT_POWER_FUSE_OFF", out _tmFusePort);
+                _simFlow.TryGetPort("tm.TMS_STATE", out _tmStatePort);
+                break;
+
+            case TrainCarType.LocoDH4:
+                _simFlow.TryGetPort("coolant.TEMPERATURE", out _tempPort);
+                _simFlow.TryGetPort("hydroDynamicBrake.EXT_IN", out _dynBrakePort);
+
+                _simFlow.TryGetPort("fluidCoupler.TURBINE_RPM", out _fcRpmPort);
+                _simFlow.TryGetPort("fluidCoupler.TURBINE_RPM_NORMALIZED", out _fcRpmNormPort);
+                _simFlow.TryGetPort("fluidCoupler.IS_BROKEN", out _fcBrokenPort);
+                _simFlow.TryGetPort("fluidCoupler.ACTIVE_CONFIGURATION", out _fcActiveConfigPort);
+                _simFlow.TryGetPort("fluidCoupler.EFFICIENCY", out _fcEfficiencyPort);
+                break;
+
+            default:
+                // TODO: LOG ERROR
+                // very bad happened
+                throw new ArgumentOutOfRangeException(nameof(loco.carType),
+                    "Unrecognized locomotives type! Got type \"" + loco.carType + '"');
+        }
+    }
+
+    public bool Equals(LocoWrapper? other)
+    {
+        if (other is null) return false;
+        return _loco.CarGUID == other._loco.CarGUID;
+    }
+
+    private void OnTrainsetChanged(Trainset a)
+    {
+        Trainset = a;
+    }
+
+    public bool Derailed => _loco.derailed;
+    public bool Exploded => _loco.isExploded;
+    public string ID => _loco.ID;
+    public float Speed => _loco.GetForwardSpeed();
+
+    public float Brake
+    {
+        get => _brakePort.Value;
+        set => _brakePort.Value = value;
+    }
+
+    public float EngineOn
+    {
+        get => _engineOnPort.Value;
+        set => _engineOnPort.Value = value;
+    }
+
+    public float FuelConsumption => _fuelConsumptionPort.Value;
+
+    public float MaxRpm => _maxRpmPort.Value;
+
+    public float Rpm => _rpmPort.Value;
+
+    public float RpmNorm => _rpmNormPort.Value;
+
+    public float FuelNorm => _fuelNormPort.Value;
+
+    public float FuelCap => _fuelCapPort.Value;
+
+    public float IndBrake
+    {
+        get => _indBrakePort.Value;
+        set => _indBrakePort.Value = value;
+    }
+
+    public float OilNorm => _oilNormPort.Value;
+
+    public float OilCap => _oilCapPort.Value;
+
+    public float Reverser
+    {
+        get => _reverserPort.Value;
+        set => _reverserPort.Value = value;
+    }
+
+    public float SandNorm => _sandNormPort.Value;
+
+    public float SandCap => _sandCapPort.Value;
+
+    public float Sander
+    {
+        get => _sanderPort.Value;
+        set => _sanderPort.Value = value;
+    }
+
+    public float Throttle
+    {
+        get => _throttlePort.Value;
+        set => _throttlePort.Value = value;
+    }
+
+    public float Temp => _tempPort.Value;
+
+    public float TmAmps
+    {
+        get
+        {
+            if (_tmAmpsPort != null) return _tmAmpsPort.Value;
+            return -1;
+        }
+    }
+
+    public float TmAmpsNorm
+    {
+        get
+        {
+            if (_tmAmpsNormPort != null) return _tmAmpsNormPort.Value;
+            return -1;
+        }
+    }
+
+    public float TmMaxAmps
+    {
+        get
+        {
+            if (_tmMaxAmpsPort != null) return _tmMaxAmpsPort.Value;
+            return -1;
+        }
+    }
+
+    public float TmRpm
+    {
+        get
+        {
+            if (_tmRpmPort != null) return _tmRpmPort.Value;
+            return -1;
+        }
+    }
+
+    public float TmRpmNorm
+    {
+        get
+        {
+            if (_tmRpmNormPort != null) return _tmRpmNormPort.Value;
+            return -1;
+        }
+    }
+
+    public float TmFuse
+    {
+        get
+        {
+            if (_tmFusePort != null) return _tmFusePort.Value;
+            return -1;
+        }
+    }
+
+    public float TmState
+    {
+        get
+        {
+            if (_tmStatePort != null) return _tmStatePort.Value;
+            return -1;
+        }
+    }
+
+    public float DynBrake
+    {
+        get
+        {
+            if (_dynBrakePort != null) return _dynBrakePort.Value;
+            return -1;
+        }
+        set
+        {
+            if (_dynBrakePort != null) _dynBrakePort.Value = value;
+        }
+    }
+
+    public float FcRpm
+    {
+        get
+        {
+            if (_fcRpmPort != null) return _fcRpmPort.Value;
+            return -1;
+        }
+    }
+
+    public float FcRpmNorm
+    {
+        get
+        {
+            if (_fcRpmNormPort != null) return _fcRpmNormPort.Value;
+            return -1;
+        }
+    }
+
+    public float FcBroken
+    {
+        get
+        {
+            if (_fcBrokenPort != null) return _fcBrokenPort.Value;
+            return -1;
+        }
+    }
+
+    public float FcActiveConfig
+    {
+        get
+        {
+            if (_fcActiveConfigPort != null) return _fcActiveConfigPort.Value;
+            return -1;
+        }
+    }
+
+    public float FcEfficiency
+    {
+        get
+        {
+            if (_fcEfficiencyPort != null) return _fcEfficiencyPort.Value;
+            return -1;
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using BepInEx;
+using DV.ThingTypes;
+using UnityEngine;
+
+namespace MyFirstPlugin;
+
+[BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+public class Plugin : BaseUnityPlugin
+{
+    private bool _loaded;
+    private LocoWrapper? _currentLoco;
+
+    private readonly List<LocoWrapper> _locos = new();
+
+    private void Awake()
+    {
+        // Plugin startup logic
+        Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+        WorldStreamingInit.LoadingFinished += OnLoadingFinished;
+        PlayerManager.CarChanged += OnCarChanged;
+        UnloadWatcher.UnloadRequested += OnUnloadRequested;
+    }
+
+    private void OnGUI()
+    {
+        if (!_loaded) return;
+
+        var richTextStyle = new GUIStyle
+        {
+            richText = true
+        };
+
+        GUILayout.BeginArea(new Rect(0, 0, 1000, 800), "Distributed Power", "box");
+
+        GUILayout.BeginHorizontal("Locomotives", GUIStyle.none);
+
+        if (_locos.Count == 0)
+            GUILayout.Label("None registered");
+        else
+        {
+            foreach (var loco in _locos)
+            {
+                GUILayout.BeginVertical();
+
+                GUILayout.Label("Properties");
+
+                GUILayout.Label("ID: " + loco.ID);
+                GUILayout.Label("Type: " + loco.Type);
+                GUILayout.Label("Derailed: " + loco.Derailed);
+                GUILayout.Label("Exploded: " + loco.Exploded);
+                GUILayout.Label("Speed: " + loco.Speed);
+
+                GUILayout.Label("Brake: " + loco.Brake);
+                GUILayout.Label("Engine On: " + loco.EngineOn);
+                GUILayout.Label("Fuel Consumption: " + loco.FuelConsumption);
+                GUILayout.Label("Max RPM: " + loco.MaxRpm);
+                GUILayout.Label("RPM: " + loco.Rpm);
+                GUILayout.Label("Normalized RPM: " + loco.RpmNorm);
+                GUILayout.Label("Normalized Fuel: " + loco.FuelNorm);
+                GUILayout.Label("Fuel Capacity: " + loco.FuelCap);
+                GUILayout.Label("Individual Brake: " + loco.IndBrake);
+                GUILayout.Label("Oil Level Normalized: " + loco.OilNorm);
+                GUILayout.Label("Oil Capacity: " + loco.OilCap);
+                GUILayout.Label("Reverser: " + loco.Reverser);
+                GUILayout.Label("Sand Level Normalized: " + loco.SandNorm);
+                GUILayout.Label("Sand Capacity: " + loco.SandCap);
+                GUILayout.Label("Sander: " + loco.Sander);
+                GUILayout.Label("Throttle: " + loco.Throttle);
+                GUILayout.Label("Temperature: " + loco.Temp);
+
+                if (loco.Type is TrainCarType.LocoShunter or TrainCarType.LocoDiesel)
+                {
+                    GUILayout.Label("Traction Motor Amps: " + loco.TmAmps);
+                    GUILayout.Label("Normalized Traction Motor Amps: " + loco.TmAmpsNorm);
+                    GUILayout.Label("Max Traction Motor Amps: " + loco.TmMaxAmps);
+                    GUILayout.Label("Traction Motor RPM: " + loco.TmRpm);
+                    GUILayout.Label("Normalized Traction Motor RPM: " + loco.TmRpmNorm);
+                    GUILayout.Label("Traction Motor Fuse: " + loco.TmFuse);
+                    GUILayout.Label("Traction Motor State: " + loco.TmState);
+                }
+
+                if (loco.Type is TrainCarType.LocoDiesel or TrainCarType.LocoDH4)
+                    GUILayout.Label("Dynamic Brake: " + loco.DynBrake);
+
+                if (loco.Type is TrainCarType.LocoDH4)
+                {
+                    GUILayout.Label("Fluid Coupler RPM: " + loco.FcRpm);
+                    GUILayout.Label("Normalized Fluid Coupler RPM: " + loco.FcRpmNorm);
+                    GUILayout.Label("Fluid Coupler Broken: " + loco.FcBroken);
+                    GUILayout.Label("Fluid Coupler Active Config: " + loco.FcActiveConfig);
+                    GUILayout.Label("Fluid Coupler Efficiency: " + loco.FcEfficiency);
+                }
+
+                if (GUILayout.Button("Unregister"))
+                {
+                    _locos.Remove(loco);
+                }
+
+                GUILayout.EndVertical();
+            }
+        }
+
+        if (_locos.Count < 6)
+        {
+            if (GUILayout.Button("Register"))
+            {
+                if (_currentLoco is not null && !_locos.Contains(_currentLoco))
+                {
+                    Logger.LogInfo("\"Register\" button pressed. Registering new locomotive...");
+                    _locos.Add(_currentLoco);
+                }
+            }
+        }
+
+        GUILayout.EndHorizontal();
+
+        GUILayout.EndArea();
+    }
+
+    private void OnLoadingFinished()
+    {
+        if (PlayerManager.Car is not null &&
+            PlayerManager.Car.carType is TrainCarType.LocoShunter or TrainCarType.LocoDiesel or TrainCarType.LocoDH4)
+        {
+            _currentLoco = new LocoWrapper(PlayerManager.Car);
+        }
+
+        _loaded = true;
+    }
+
+    private void OnUnloadRequested()
+    {
+        _currentLoco = null;
+        _loaded = false;
+    }
+
+    private void OnCarChanged(TrainCar? car)
+    {
+        if (car is null)
+        {
+            _currentLoco = null;
+            Logger.LogInfo("Loco changed to null.");
+        }
+        else if (car.carType is TrainCarType.LocoShunter or TrainCarType.LocoDiesel or TrainCarType.LocoDH4)
+        {
+            _currentLoco = new LocoWrapper(car);
+            Logger.LogInfo("Loco changed. ID: " + _currentLoco.ID + "; Type: " + _currentLoco.Type);
+        }
+    }
+
+    private void Update()
+    {
+        if (!_loaded || _currentLoco is null || !_locos.Contains(_currentLoco)) return;
+        
+        // hopefully _currentLoco doesn't change in a frame (it shouldn't) (source: my ass)
+        foreach (var loco in _locos.Where(loco => loco != _currentLoco))
+        {
+            loco.Throttle = _currentLoco.Throttle;
+            loco.Brake = _currentLoco.Brake;
+            loco.IndBrake = _currentLoco.IndBrake;
+            loco.Sander = _currentLoco.Sander;
+
+            if (loco.DynBrake > -1 && _currentLoco.DynBrake > -1)
+            {
+                loco.DynBrake = _currentLoco.DynBrake;
+            }
+
+            bool reversed = (loco.Speed * _currentLoco.Speed) < 0;
+            if (reversed)
+            {
+                loco.Reverser = 1 - _currentLoco.Reverser;
+            }
+            else
+            {
+                loco.Reverser = _currentLoco.Reverser;
+            }
+        }
+    }
+}
